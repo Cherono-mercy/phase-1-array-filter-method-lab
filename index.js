7f7fe397b23b2184fcc1184149de45d8cf435984
Mercy Cherono <cheronom142@gmail.com>
2023-09-26 15:57:16 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+const { arrayContaining } = require("expect");
+
+
+function findMatching(drivers, strings){
+   return drivers.filter ((names) => 
+ names.toLowerCase() === strings.toLowerCase()
+   )
+}
+ 
+function fuzzyMatch(drivers, strings){
+  return drivers.filter ((names) => 
+  names.slice(0,2) === strings.slice(0,2))
+}
+
+function matchName(drivers, strings){
+    return drivers.filter ((names) =>
+    names.name === strings 
+    )
+}
+
